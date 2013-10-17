2ce99c59472a09a735a7791091d821117ecfbc48
Miguel Ángel García <miguel.garcia@akamon.me>
2013-10-17 15:30:02 +0200

    API-110 Mapped all the proxy user operations

---
+++ src/com/akamon/api/client/proxy/user/UserProxy.java
@@ -0,0 +1,219 @@
+package com.akamon.api.client.proxy.user;
+
+import com.akamon.api.client.error.ServiceInvocationException;
+import com.akamon.api.client.proxy.BaseServiceProxy;
+import com.akamon.api.client.security.AuthData;
+import com.akamon.api.client.service.ICallableResponse;
+import com.akamon.api.client.service.error.ServiceDefinitionException;
+import com.akamon.api.client.service.imp.http.JsonCallableResponse;
+import java.util.HashMap;
+
+/**
+ * Proxy object to invoke the user operations 
+ * @author Miguel Angel Garcia
+ */
+public class UserProxy extends BaseServiceProxy { 
+                    
+    public UserProxy(AuthData authData){
+        super(authData);
+    }
+    
+    /**
+     * Create a new user accouunt
+     * @param name (Required)  User name
+     * @param password (Required)  User password
+     * @param email (Required) email
+     * @param genre (Required)  User genre. V for man, H for woman and I when it's unknown
+     * @param partnerAlias (Required) Partner alias (www for www.mundijuegos.com)
+     * @param ip User ip
+     * @param country Two letter iso country code
+     * @param friend Username of the friend that suggested this registration
+     * @param additionalParams json-code-key-valued-array with special params, for example campaign params (utm_medium, utm_term, utm_content, utm_campaign, utm_source, ..)
+     * @return User created data
+     */
+    public UserResponseData registerWebUser(String name, 
+            String password, 
+            String email,
+            String genre,
+            String partnerAlias,
+            String ip,
+            String country,
+            String friend,
+            HashMap<String,String> additionalParams) throws ServiceDefinitionException, ServiceInvocationException
+    {
+        UserResponseData response = null;
+        Object[] params = {name, 
+            password, 
+            email, 
+            genre, 
+            partnerAlias, 
+            ip,
+            country,
+            friend,
+            additionalParams};
+        ICallableResponse res = invoke("registerWebUser", params);
+        if (res instanceof JsonCallableResponse){
+            JsonCallableResponse jRes = (JsonCallableResponse) res;
+            
+            try {
+                response = jRes.buildResponseDataObject(UserResponseData.class);            
+            }
+            catch (Exception ex){
+                 throw new ServiceInvocationException("registerWebUser", "Response parse error", ex);
+            }
+        }
+        
+        return response;
+    }
+    
+    /**
+     * Create a new user accouunt
+     * @param name (Required) User name
+     * @param password (Required) User password
+     * @param email (Required) email
+     * @param genre (Required) User genre. V for man, H for woman and I when it's unknown
+     * @param partnerAlias (Required) Partner alias (www for www.mundijuegos.com)    
+     * @return User created data
+     */
+    public UserResponseData registerWebUser(String name, 
+            String password, 
+            String email,
+            String genre,
+            String partnerAlias) throws ServiceDefinitionException, ServiceInvocationException
+    {
+        return registerWebUser(name, password, email, genre, partnerAlias, null, null, null, null);
+    }
+    
+    /**
+     * Links a user from an external site (for example, facebook). If the user has not been linked
+     * before, a new account at our side it's created. If not, the before created user reference
+     * is returned
+     *      
+     * @param providerName (Required)Provider name. It must be facebook or taringa
+     * @param providerUserId (Required) User id in the external system (in the case of facebook, user id)
+     * @param email (Required) User email
+     * @param genre (Required) User genre (V for man, H for woman, I for not defined yet)
+     * @param ip (Required) User ip (number dotted string format)
+     * @param name User desired name (5-15 letters and numbers string)
+     * @param password User desired password
+     * @param country two-letter iso country code
+     * @param partnerAlias Alias of the partner (www by default)
+     * @param friend User name from the friend that enrolled this user
+     * @param additionalParams key-valued structure with special params :
+     *  channel => ‘Facebook’, ‘Portal’, ‘IOS’, ‘Android’
+     *  game_id => Game id responsible for the user register
+     *  bu_lang => user country in the ISO 3166 two letter country code
+     *  utm_medium => utm campaign parameter
+     *  utm_term => utm campaign parameter
+     *  utm_source => utm campaign parameter 
+     *  utm_content => utm campaign parameter
+     * @param realName User real name (for example: 'John Smith'). It helps to generate the user loginname, when is not possible to do it from his email
+     * @return User data
+     * @throws ServiceDefinitionException
+     * @throws ServiceInvocationException 
+     */
+    public UserResponseData registerOrLoginExternalUser(String providerName, 
+            String providerUserId, 
+            String email, 
+            String genre, 
+            String ip, 
+            String name, 
+            String password, 
+            String country, 
+            String partnerAlias, 
+            String friend, 
+            HashMap<String,String> additionalParams, 
+            String realName) throws ServiceDefinitionException, ServiceInvocationException
+    {
+        UserResponseData response = null;
+        
+        Object[] params = {providerName,
+            providerUserId,
+            name,email,
+            password,
+            genre,
+            ip,
+            country,
+            partnerAlias,
+            friend,
+            additionalParams,
+            realName};
+        
+        ICallableResponse res = invoke("registerOrLoginExternalUser", params);
+        if (res instanceof JsonCallableResponse){
+            JsonCallableResponse jRes = (JsonCallableResponse) res;
+            
+            try {
+                response = jRes.buildResponseDataObject(UserResponseData.class);            
+            }
+            catch (Exception ex){
+                 throw new ServiceInvocationException("registerWebUser", "Response parse error", ex);
+            }
+        }
+        
+        return response;                
+    }
+    
+    /**
+     * Links a user from an external site (for example, facebook). If the user has not been linked
+     * before, a new account at our side it's created. If not, the before created user reference
+     * is returned
+     *      
+     * @param providerName (Required)Provider name. It must be facebook or taringa
+     * @param providerUserId (Required) User id in the external system (in the case of facebook, user id)
+     * @param email (Required) User email
+     * @param genre (Required) User genre (V for man, H for woman, I for not defined yet)
+     * @param ip (Required) User ip (number dotted string format)
+     * @return User data
+     * @throws ServiceDefinitionException
+     * @throws ServiceInvocationException 
+     */
+    public UserResponseData registerOrLoginExternalUser(String providerName, 
+            String providerUserId, 
+            String email, 
+            String genre, 
+            String ip) throws ServiceDefinitionException, ServiceInvocationException
+    {
+        return registerOrLoginExternalUser(providerName, 
+            providerUserId, 
+            email, 
+            genre, 
+            ip, 
+            null, 
+            null, 
+            null, 
+            null, 
+            null, 
+            null, 
+            null);
+    }
+    
+    /**
+     * Authenticates an user by its username and password
+     * @param userName (Required) User name
+     * @param password (Required) User password
+     * @return User data
+     * @throws ServiceDefinitionException
+     * @throws ServiceInvocationException 
+     */
+    public UserResponseData authenticateUser(String userName, String password) throws ServiceDefinitionException, ServiceInvocationException {
+        UserResponseData response = null;
+        
+        Object[] params = {userName, password};
+        
+        ICallableResponse res = invoke("authenticateUser", params);
+        if (res instanceof JsonCallableResponse){
+            JsonCallableResponse jRes = (JsonCallableResponse) res;
+            
+            try {
+                response = jRes.buildResponseDataObject(UserResponseData.class);            
+            }
+            catch (Exception ex){
+                 throw new ServiceInvocationException("authenticateUser", "Response parse error", ex);
+            }
+        }
+        
+        return response;   
+    }
+    
+}
---
+++ src/com/akamon/api/client/proxy/user/UserResponseData.java
@@ -0,0 +1,121 @@
+package com.akamon.api.client.proxy.user;
+
+/**
+ * Bean to encaosulate the user data  obtained from server
+ * @author Miguel Angel Garcia
+ */
+public class UserResponseData {
+    /**
+     * Public user identifier to use in all the calls
+     */
+    private String public_user_id;
+    
+    /**
+     * User name
+     */
+    private String user_name;
+    
+    /**
+     * Private user id (for internal use only)
+     */
+    private Integer user_id;
+    
+    /**
+     * Avatar to obtain the user url
+     */
+    private String avatar_url;
+    
+    /**
+     * User genre: V for man, H for woman, or I for unknown
+     */
+    private String genre;
+    
+    /**
+     * User chips balance
+     */
+    private Integer chips;
+
+    /**
+     * @return the public_user_id
+     */
+    public String getPublic_user_id() {
+        return public_user_id;
+    }
+
+    /**
+     * @param public_user_id Public user identifier to use in all the calls to set
+     */
+    public void setPublic_user_id(String public_user_id) {
+        this.public_user_id = public_user_id;
+    }
+
+    /**
+     * @return User name
+     */
+    public String getUser_name() {
+        return user_name;
+    }
+
+    /**
+     * @param user_name User name
+     */
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    /**
+     * @return Private user id (intern usage only)
+     */
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    /**
+     * @param user_id Private user id (intern usage only)
+     */
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    /**
+     * @return rivate user id (intern usage only)
+     */
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    /**
+     * @param avatar_url User avatar_url
+     */
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    /**
+     * @return User genre (V for man, H for woman, and I when is not known)
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre User genre (V for man, H for woman, and I when is not known)
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return User chips balance
+     */
+    public Integer getChips() {
+        return chips;
+    }
+
+    /**
+     * @param User chips balance
+     */
+    public void setChips(Integer chips) {
+        this.chips = chips;
+    }        
+}
