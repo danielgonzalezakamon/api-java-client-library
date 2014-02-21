a88e68d690654b5435fc3344ae32c3145302c9db
miguel.garcia <miguel.garcia@akamon.me>
2014-02-21 12:26:10 +0100

    API-150 add test to check the getFacebookUserId() UserData method

---
+++ test/com/akamon/api/client/proxy/user/UserResponseDataTest.java
@@ -0,0 +1,42 @@
+package com.akamon.api.client.proxy.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author miguelgarcia
+ */
+public class UserResponseDataTest {
+    
+    @Test
+    public void testGetFacebookId()
+    {
+        String expectedFacebookUserId = "10";
+        UserResponseData responseData = new UserResponseData();
+        List <UserLinkFromProviderResponseData> linKFromProviders = 
+                createUserLinkFromProvider(UserResponseData.PROVIDER_FACEBOOK, expectedFacebookUserId);
+        
+        responseData.setLinks_from_providers(linKFromProviders);
+        
+        String obtainedFacebookUserId = responseData.getFacebookUserId();
+        
+        assertEquals("Facebook user does not match", expectedFacebookUserId, obtainedFacebookUserId);
+    }
+    
+    private List <UserLinkFromProviderResponseData>  createUserLinkFromProvider(String providerName, String providerUserId)
+    {
+        List <UserLinkFromProviderResponseData> linKFromProviders = new ArrayList();
+        UserLinkFromProviderResponseData providerData = new UserLinkFromProviderResponseData();
+        providerData.setProvider(providerName);
+        providerData.setProvider_user_id(providerUserId);        
+        
+        linKFromProviders.add(providerData);
+        
+        return linKFromProviders;        
+    }
+    
+}
